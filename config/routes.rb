2f7c58999daa88a23c7d2f33c4d17785548109c6
dreamlx <dreamlinx@gmail.com>
2014-04-23 23:49:02 +0800

    add api

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,11 @@ Houseinfo2::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  namespace :api do
+    resources :builders
+    resources :units
+    resources :unit_models
+  end
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
