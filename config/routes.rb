f056b865912eb7b155674fa0ff3085227eb7d7f9
dreamlx <dreamlinx@gmail.com>
2014-04-25 02:00:47 +0800

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Houseinfo2::Application.routes.draw do
   end
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'admin/dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
