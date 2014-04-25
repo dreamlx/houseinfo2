e89fff6585f26e7314ea49d4799394597886b02f
dreamlx <dreamlinx@gmail.com>
2014-04-25 23:58:07 +0800

    fix

--- db/migrate/20140425154246_change_room_no_format_in_room.rb
+++ db/migrate/20140425154246_change_room_no_format_in_room.rb
@@ -1,6 +1,6 @@
 class ChangeRoomNoFormatInRoom < ActiveRecord::Migration
   def up
-    change_column :rooms, :room_no, :integer
+    change_column :rooms, :room_no, :integer, 'integer USING CAST(room_no AS integer)'
   end
 
   def down
