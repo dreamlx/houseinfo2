88b3056b1eee702cff76310bf17a453f6c28292c
ken-wong <wangkun025@gmail.com>
2014-06-01 12:56:58 +0800

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.destroy_all
-AdminUser.create!(email: 'admin@gmail.com', password: '11111111')
+AdminUser.create!(name: 'admin', email: 'admin@gmail.com', password: '11111111', role: 'manager')
 Room.update_all(state: 'on_sale')
